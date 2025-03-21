dcbb6eeffbfb1c2a53889ec030cd3b8bbd0117c4
MohamedAbouzaid <mo.adel.developer@gmail.com>
2025-03-21 05:59:33 +0200

    add default sidebar

---
+++ src/ui/components/DefaultAppSidebar.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { Sidebar, SidebarContent, SidebarHeader, useSidebar } from "@/components/ui/sidebar";
+import { RoutesName } from "@/core/utils/constants";
+import { Loader, User } from "lucide-react";
+import { useSession } from "next-auth/react";
+import AppImage from "../atoms/media/AppImage";
+import { NavMain } from "./NavMain";
+
+
+// This is sample data.
+const navMain = [
+  {
+    title: "البيانات الشخصية",
+    url: RoutesName.editProfile,
+    icon: User
+  }
+]
+
+export function DefaultAppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const {open} = useSidebar()
+  const { data: session, status } = useSession() as { data: { user: { image: string, name: string } } | null, status: string };
+  return (
+    <Sidebar collapsible="icon" {...props} side="right">
+      <SidebarHeader className="bg-primary">
+        <div className="flex flex-col justify-center items-center gap-2">
+        <div className="border-secondary shadow-xl border-2 p-1 rounded-full">
+        {
+          status == 'loading' || !session ?
+          <Loader /> :
+          <AppImage src={session.user.image} alt="user" width={80} height={80} className="bg-white rounded-full shadow-lg" />
+        }
+        </div>
+        {
+          (open == true && status == 'authenticated') && <h3 className="text-white ms-3 text-sm font-bold">{session?.user.name}</h3>
+        }
+        </div>
+      </SidebarHeader>
+      <SidebarContent className="bg-background">
+        <NavMain items={navMain} />
+      </SidebarContent>
+    </Sidebar>
+  )
+}
